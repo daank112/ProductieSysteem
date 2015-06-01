4ce5cdbfe0de8c162af4bc7424cac6dec2c01dbf
daank112 <daankleindop@hotmail.com>
2015-06-01 18:32:11 +0200

    Standaard bestanden toegevoegd!

---
+++ ProductieSysteemV1.0/ProductieSysteemV1.0/Controllers/ProductionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductieSysteemV1._0.Controllers
+{
+    public class ProductionController : Controller
+    {
+        // GET: Produciton
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProductieSysteemV1.0/ProductieSysteemV1.0/Models/DayViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductieSysteemV1._0.Models
+{
+    public class DayViewModel
+    {
+    }
+}
